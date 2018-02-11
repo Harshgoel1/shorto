540177d3c0bdd6650adca31fdf13f40637b9fbf5
Ashish Nagpal <an6285@gmail.com>
2018-02-11 16:12:32 +0530

    Added Files

---
+++ app.js
@@ -0,0 +1,105 @@
+var express=require('express');
+var app=express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var path=require('path');
+var base58 = require('./base58.js');
+
+var mongoose=require('mongoose');
+
+var site_name="http://localhost:3000/";
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://test:test@ds127888.mlab.com:27888/shorto',function (err){
+	if(err) throw err;
+	console.log('Database connection successful');
+});
+
+var urlschema=mongoose.Schema({
+  seq_no : {type:Number,index:true},
+  long_url : {type:String},
+  short_url : {type:String},
+  created_at: {type:Date}
+});
+
+var counterschema=mongoose.Schema({
+	seq_no : {type:Number,index:true},
+	counter : {type:Number}
+});
+
+var urls=mongoose.model('urls',urlschema);
+var counters=mongoose.model('counters',counterschema);
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/:encoded_id', function(req, res){
+  var base58Id = req.params.encoded_id;
+  var id = base58.decode(base58Id);
+
+  urls.findOne({seq_no: id}, function (err, urls){
+    if (urls) {
+      res.redirect(urls.long_url);
+    } else {
+      res.render('error');
+    }
+  });
+
+});
+
+app.post('/create',function(req,res){
+  var long_url=req.body.urlfield;
+  var short_url="";
+  var date=new Date();
+	var new_seq_no;
+
+
+	urls.findOne({'long_url':long_url},function(err,result){
+
+		if (err) throw err;
+		console.log(result);
+		if(result!=null)
+		{
+			console.log('Url already exists');
+			console.log(result.short_url);
+			res.render('url',{short_url:site_name+result.short_url});
+		}
+		else
+		{
+			counters.findOne({seq_no: 1}, function(err,counters){
+		    if(counters){
+		        counters.counter += 1
+		        counters.save(function(err) {
+		            if (err) throw err;
+		        });
+		    }else{
+		        console.log(err);
+		    }
+			});
+
+			counters.findOne({seq_no: 1},function(err,counters){
+				short_url=base58.encode(counters.counter);
+				var insert=urls({seq_no:counters.counter,long_url:long_url,short_url:short_url,created_at:date}).save(function(err){
+					if (err) throw err;
+					console.log('Url inserted');
+					console.log('Short Url'+short_url);
+					res.render('url',{short_url:site_name+short_url});
+				});
+			});
+		}
+	});
+
+});
+
+var server=app.listen(3000,function()
+{
+  console.log("Server started successfully on port 3000");
+});
